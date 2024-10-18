7036f30090f4323c7932c0f661622729b606f07a
Mikael <bernardesmikael@outlook.com>
2024-10-18 17:43:16 -0300

    feat: add validation in zod

--- src/app/(auth)/signup/address/page.tsx
+++ src/app/(auth)/signup/address/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@UI/button";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@UI/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@UI/form";
 import { Input } from "@UI/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
@@ -22,140 +22,89 @@ export default function Address() {
 
 	const onSubmit = async (data: AddressSignupTypeSchema) => {
 		formContext.setValue("addressSignupSchema", data);
-		push("/signup/address");
+		push("/signin");
 	};
 
 	return (
-		<form
-			onSubmit={form.handleSubmit(onSubmit)}
-			className="w-full flex flex-col gap-4">
-			<div className="flex gap-4">
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="w-full flex flex-col gap-4">
+				<div className="flex gap-4">
+					<FormField
+						control={form.control}
+						name="zipCode"
+						render={({ field }) => (
+							<FormItem className="w-full max-w-24">
+								<FormLabel>CEP</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="00000-000"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="country"
+						render={({ field }) => (
+							<FormItem className="w-full">
+								<FormLabel>País</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="Ex: Brasil"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<div className="flex gap-4">
+					<FormField
+						control={form.control}
+						name="state"
+						render={({ field }) => (
+							<FormItem className="w-full max-w-20">
+								<FormLabel>UF</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="Ex: SP"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="city"
+						render={({ field }) => (
+							<FormItem className="w-full">
+								<FormLabel>Cidade</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="Ex: São Paulo"
+										type="password"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
 				<FormField
 					control={form.control}
-					name="zipCode"
+					name="neighborhood"
 					render={({ field }) => (
-						<FormItem className="w-full max-w-24">
-							<FormLabel>CEP</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="00000-000"
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="country"
-					render={({ field }) => (
-						<FormItem className="w-full">
-							<FormLabel>País</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="Ex: Brasil"
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-			</div>
-			<div className="flex gap-4">
-				<FormField
-					control={form.control}
-					name="state"
-					render={({ field }) => (
-						<FormItem className="w-full max-w-20">
-							<FormLabel>UF</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="Ex: SP"
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="city"
-					render={({ field }) => (
-						<FormItem className="w-full">
-							<FormLabel>Cidade</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="Ex: São Paulo"
-									type="password"
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-			</div>
-			<FormField
-				control={form.control}
-				name="neighborhood"
-				render={({ field }) => (
-					<FormItem>
-						<FormLabel>Bairro</FormLabel>
-						<FormControl>
-							<Input
-								placeholder="Confirme sua senha"
-								type="password"
-								{...field}
-							/>
-						</FormControl>
-						<FormMessage />
-					</FormItem>
-				)}
-			/>
-			<FormField
-				control={form.control}
-				name="street"
-				render={({ field }) => (
-					<FormItem>
-						<FormLabel>Rua</FormLabel>
-						<FormControl>
-							<Input
-								placeholder="Confirme sua senha"
-								type="password"
-								{...field}
-							/>
-						</FormControl>
-						<FormMessage />
-					</FormItem>
-				)}
-			/>
-			<div className="flex gap-4">
-				<FormField
-					control={form.control}
-					name="streetNumber"
-					render={({ field }) => (
-						<FormItem className="w-full max-w-16">
-							<FormLabel>N°</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="0000"
-									type="password"
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="complement"
-					render={({ field }) => (
-						<FormItem className="w-full">
-							<FormLabel>Complemento</FormLabel>
+						<FormItem>
+							<FormLabel>Bairro</FormLabel>
 							<FormControl>
 								<Input
 									placeholder="Confirme sua senha"
@@ -167,8 +116,61 @@ export default function Address() {
 						</FormItem>
 					)}
 				/>
-			</div>
-			<Button type="submit">Avançar</Button>
-		</form>
+				<FormField
+					control={form.control}
+					name="street"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Rua</FormLabel>
+							<FormControl>
+								<Input
+									placeholder="Confirme sua senha"
+									type="password"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<div className="flex gap-4">
+					<FormField
+						control={form.control}
+						name="streetNumber"
+						render={({ field }) => (
+							<FormItem className="w-full max-w-16">
+								<FormLabel>N°</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="0000"
+										type="password"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="complement"
+						render={({ field }) => (
+							<FormItem className="w-full">
+								<FormLabel>Complemento</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="Confirme sua senha"
+										type="password"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<Button type="submit">Cadastrar</Button>
+			</form>
+		</Form>
 	);
 }
--- src/app/(auth)/signup/schema.ts
+++ src/app/(auth)/signup/schema.ts
@@ -1,22 +1,73 @@
 import { z } from "zod";
 
-export const accountSignupSchema = z.object({
-	name: z.string().min(1, "Campo obrigatório"),
-	username: z.string().min(1, "Campo obrigatório"),
-	email: z.string().email(),
-	password: z.string().min(1, "Campo obrigatório"),
-	confirmPassword: z.string().min(1, "Campo obrigatório"),
-});
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const accountSignupSchema = z
+	.object({
+		name: z
+			.string()
+			.min(1, "Campo obrigatório")
+			.refine((value) => /^[a-zA-Z\s]+$/.test(value), {
+				message: "O nome não deve conter números ou caracteres especiais",
+			}),
+		username: z
+			.string()
+			.min(1, "Campo obrigatório")
+			.refine((value) => !/\s/.test(value), {
+				message: "O nome de usuário não deve conter espaços",
+			}),
+		email: z.string().email("E-mail incorreto"),
+		password: z
+			.string()
+			.min(1, "Campo obrigatório")
+			.regex(
+				passwordRegex,
+				"A senha deve ter no mínimo 8 caracteres, incluindo letras maiúsculas, minúsculas, números e um caractere especial",
+			),
+		confirmPassword: z
+			.string()
+			.min(1, "Campo obrigatório")
+			.regex(
+				passwordRegex,
+				"A senha deve ter no mínimo 8 caracteres, incluindo letras maiúsculas, minúsculas, números e um caractere especial",
+			),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "As senhas devem ser iguais",
+		path: ["confirmPassword"],
+	});
+
+const zipCodeRegex = /^\d{5}-\d{3}$/;
 
 export const addressSignupSchema = z.object({
-	zipCode: z.string().min(1, "Campo obrigatório"),
-	country: z.string().min(1, "Campo obrigatório"),
+	zipCode: z.string().min(1, "Campo obrigatório").regex(zipCodeRegex, "Formato de CEP inválido"),
+	country: z
+		.string()
+		.min(1, "Campo obrigatório")
+		.refine((value) => /^[a-zA-Z\s]+$/.test(value), {
+			message: "O nome do país não deve conter números ou caracteres especiais",
+		}),
 	state: z.string().min(1, "Campo obrigatório"),
 	city: z.string().min(1, "Campo obrigatório"),
-	neighborhood: z.string().min(1, "Campo obrigatório"),
-	street: z.string().min(1, "Campo obrigatório"),
-	streetNumber: z.string().min(1, "Campo obrigatório"),
-	complement: z.string().min(1, "Campo obrigatório"),
+	neighborhood: z
+		.string()
+		.min(1, "Campo obrigatório")
+		.refine((value) => /^[a-zA-Z\s]+$/.test(value), {
+			message: "O nome do bairro não deve conter números ou caracteres especiais",
+		}),
+	street: z
+		.string()
+		.min(1, "Campo obrigatório")
+		.refine((value) => /^[a-zA-Z\s]+$/.test(value), {
+			message: "O nome da rua não deve conter números ou caracteres especiais",
+		}),
+	streetNumber: z
+		.string()
+		.min(1, "Campo obrigatório")
+		.refine((value) => /^\d+$/.test(value), {
+			message: "Só número",
+		}),
+	complement: z.string().optional(),
 });
 
 export const signupSchema = z.object({
